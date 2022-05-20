0ae986d3ee480a18fab0610c51a4d565c842da6a
shaymuel <97071744+shaymuel@users.noreply.github.com>
2022-05-20 15:38:06 -0600

    updating route endpoints
    updating route endpoints

--- routes/moods.js
+++ routes/moods.js
@@ -5,6 +5,6 @@ const moodsController = require('../controllers/moods');
 //gets all moods
 routes.get('/', moodsController.getAllMoods);
 //gets mood by the mood name
-routes.get('/:moodName', moodsController.getMood);
+routes.get('/:id', moodsController.getMood);
 
 module.exports = routes;
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const usersController = require('../controllers/users');
 //gets all users
 routes.get('/', usersController.getAllUsers);
 //gets mood by the mood name
-routes.get('/:username', usersController.getUser);
+routes.get('/:id', usersController.getUser);
 //Add a new user
 routes.post('/', usersController.createNewUser);
 
