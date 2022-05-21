29ea4b3a4111b2d0323677cb86058c8f121f5fd4
shaymuel <97071744+shaymuel@users.noreply.github.com>
2022-05-20 23:18:12 -0600

    updating swagger.js
    updating swagger.js

--- swagger.js
+++ swagger.js
@@ -2,12 +2,25 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'Habit & Mood Tracker API',
-    description: 'Keeps track of habits made and moods felt. Recorded by user',
+    title: "Habit & Mood Tracker API",
+    description: "Keeps track of habits made and moods felt. Recorded by user",
+    version: "1.0.0"
   },
-  host: 'habit-mood-tracker.herokuapp.com',
-  schemes: ['https'],
+  host: "habit-mood-tracker.herokuapp.com",
+  basePath: "/",
+  schemes: ["https"],
+  tags: [
+    {
+      name: "moods",
+      description: "Operations for moods"
+    },
+    {
+      name: "user",
+      description: "Operations for users"
+    }
+  ]
 };
+
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./routes/index.js'];
